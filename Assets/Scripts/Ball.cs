89ff0677c00ee72bccdd5dddf5596e1640d70d69
Alexander Xu <ajm.xu@outlook.com>
2017-03-01 18:01:08 +0000

    Initial Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public Rigidbody2D hook;
+
+	public float releaseTime = 0.15f;
+	public float maxDragDistance = 2f;
+	public float minDragDistance = 0.4f;
+
+	public GameObject nextBall;
+	public Vector2 mousePos;
+
+	private bool isPressed = false;
+
+
+	void Update() {
+		if (isPressed) {
+			mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
+				rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+			else 
+				rb.position = mousePos;
+		}
+	}
+
+	void OnMouseDown() { 
+		isPressed = true;
+		rb.isKinematic = true; 
+	}
+
+	void OnMouseUp() { 
+		isPressed = false; 
+		rb.isKinematic = false;
+
+		if (Vector3.Distance(mousePos, hook.position) > minDragDistance)
+			StartCoroutine(Release());
+	}
+
+	IEnumerator Release() {
+		yield return new WaitForSeconds(releaseTime);
+
+		GetComponent<SpringJoint2D>().enabled = false;
+		this.enabled = false;
+
+		yield return new WaitForSeconds(2f);
+
+		if (nextBall != null) nextBall.SetActive(true);
+		else {
+			Enemy.EnemiesAlive = 0;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour {
+
+	public float health = 4f;
+	public GameObject deathEffect;
+
+	public static int EnemiesAlive = 0;
+
+	void Start() {
+		EnemiesAlive++;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.relativeVelocity.magnitude > health) Die();
+	}
+
+	void Die() {
+		GameObject effect = (GameObject) Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+		EnemiesAlive--;
+		if (EnemiesAlive <= 0) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+		Destroy(effect, 2f);
+		Destroy(gameObject);
+	}
+}
